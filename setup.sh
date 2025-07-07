157fe6724d135672c80dee9b54804b846a9e957d
Y0U <rubalsharma507@gmail.com>
2025-07-07 12:51:57 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,25 @@
+clear
+
+echo -e "\e[5m\e[1;34m$(figlet 'YOUSNIFFER')\e[0m"
+echo Setup Starting........
+
+sudo apt-get install ruby   
+
+
+echo ""
+sudo gem install colorize
+echo ""
+sudo gem install optparse
+echo ""
+sudo gem install packetgen
+echo ""
+sudo gem install resolv
+echo ""
+sudo cp yousniffer /usr/bin
+sudo chmod +x /usr/bin/yousniffer
+echo "\n***done***\n"
+clear
+echo ""
+echo Setup Complete GO AHEAD!!!!!!        
+echo ""
+echo "----->Now you can use youfinder from everywhere<-----"
